266980106269ad2ff0d9e0adf48a4b67014a0759
drgerger <92130033+drgerger@users.noreply.github.com>
2024-04-15 14:32:11 -0400

    navbar and rudimentary home

--- server.py
+++ server.py
@@ -1,9 +1,40 @@
-from flask import Flask
-from flask import render_template
-from flask import Response, request, jsonify
-app = Flask(__name__)
+# app.py
+
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+
+app = Flask(__name__, static_url_path='/static')
+
+current_id = 0
+data = {
+    # Data goes in here
+}
 
 @app.route('/')
-def home():
-    return render_template('homepage.html')
+def index():
+    return render_template('home.html')
 
+# @app.route('/learn')
+# def learn():
+#     return
+
+# @app.route('/quiz')
+# def quiz():
+#     return
+
+# # check navbar in examples from lecture
+# @app.route('/terms')
+# def terms():
+#     return
+ 
+# @app.route('/types')
+# def types():
+
+#     return
+
+# @app.route('/pairings')
+# def types():
+
+#     return
+
+if __name__ == '__main__':
+    app.run(debug=True)
