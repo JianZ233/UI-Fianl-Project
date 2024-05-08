f3fbcd3db07e779a37015dd5feccf6c48da15d80
JianZ233 <136143373+JianZ233@users.noreply.github.com>
2024-05-07 22:41:24 -0400

    redesigned homepage

---
+++ static/actions.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        effect: 'fade',
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
+}
